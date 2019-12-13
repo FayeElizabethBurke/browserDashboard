e6ad303fdfdb521f8c9e18cabd60122d137c4dc2
Faye Burke <fayeelizabethburke@gmail.com>
2019-12-13 14:59:22 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios';
+
+export default class apis extends React.Component {
+  state = {
+    image: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=nEw4cQXLDEnerOgiB1nvFzHr4aJMa8BeOXOpVbLd`)
+      .then(res => {
+        const image = res.data.hdurl;
+        this.setState({ image });
+      })
+  }
+
+  render() {
+    const containerStyle = {
+      margin: '0',
+      width: '100%',
+      height: '800px',
+      backgroundImage: `url(${this.state.image})`,
+      opacity: `0.8`
+    }
+    return (
+      // <ul>
+      //   { this.state.image.map(person => <li>{person.address.suite}</li>)}
+      // </ul>
+      <div style={containerStyle}>
+        <ul>
+        <li>{this.state.image}</li>
+      </ul>
+      </div>
+
+    )
+  }
 }
-
-export default App;
