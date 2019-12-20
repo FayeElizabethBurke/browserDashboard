ef03ae3ce41a1cac02c0b5049d311bfb39b4369d
Faye Burke <fayeelizabethburke@gmail.com>
2019-12-20 16:01:38 +1000

    added explanation api call

--- src/App.js
+++ src/App.js
@@ -4,17 +4,25 @@ import axios from 'axios';
 
 export default class apis extends React.Component {
   state = {
-    image: []
+    image: [],
+    explanation: []
   }
 
   componentDidMount() {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=nEw4cQXLDEnerOgiB1nvFzHr4aJMa8BeOXOpVbLd`)
       .then(res => {
-        const image = res.data.hdurl;
+        const image = res.data.url;
         this.setState({ image });
       })
   }
 
+  componentDidMount() {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=nEw4cQXLDEnerOgiB1nvFzHr4aJMa8BeOXOpVbLd`)
+      .then(res => {
+        const explanation = res.data.explanation;
+        this.setState({ explanation });
+      })
+  }
   render() {
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     var today = new Date();
@@ -49,7 +57,9 @@ var today = dd + ` ` + months[mm] + ' ' + yyyy;
       <div style={containerStyle}>
         <h1 style = {headingStyle}>Hello, Faye</h1>
     <h2 style={smallHeadingStyle}>It's {today}</h2>
+    <p>{this.state.explanation}</p>
       </div>
+
     )
   }
 }
