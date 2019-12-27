efc02bf0aab152c7c0d0b02d92f59fc6e5922c0b
Faye Burke <fayeelizabethburke@gmail.com>
2019-12-27 17:33:03 +1000

    modify for chrome extens.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import Iframe from 'react-iframe';
 //google api key: AIzaSyBH0_ZkBlm2PMYa2XIjFp_fqGEpzYJuYDQ 
 
+
 export default class apis extends React.Component {
   state = {
     image: [],
@@ -12,7 +13,7 @@ export default class apis extends React.Component {
   componentDidMount() {
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=nEw4cQXLDEnerOgiB1nvFzHr4aJMa8BeOXOpVbLd`)
       .then(res => {
-        const image = res.data.url;
+        const image = res.data.hdurl;
         this.setState({ image });
         const explanation = res.data.explanation;
         this.setState({ explanation });
@@ -29,43 +30,59 @@ var yyyy = today.getFullYear();
 if (dd < 10) {
   dd = '0' + dd;
 } 
-var today = dd + ` ` + months[mm] + ' ' + yyyy;
+today = dd + ` ` + months[mm] + ' ' + yyyy;
     const containerStyle = {
       margin: '0',
-      width: '100%',
-      height: '800px',
+      width: '98vw',
+      height: '100vw',
       backgroundImage: `url(${this.state.image})`,
-      opacity: `0.8`
+      webkitFilter: `invert(1)`,
+      filter: `invert(1)`,
+      backgroundRepeat: `no-repeat`,
+      backgroundSize: `100%`,
+      textAlign: `center`
     }
     const headingStyle = {
-      fontSize: `80px`,
+      fontSize: `120px`,
       opacity: 1 + ` !important`,
       textAlign: `center`,
-      paddingTop: `15%`,
-      color: 'red'
+      paddingBottom: `15%`,
+      color: 'white',
+      margin: `0`,
+      paddingTop: `10%`,
     }
     const smallHeadingStyle = {
-      fontSize: `50px`,
+      fontSize: `40px`,
       opacity: 1 + ` !important`,
-      textAlign: `center`,
       paddingTop: `2%`,
-      color: 'red'
+      paddingBottom: `5%`,
+      color: 'white',
+      textAlign: `left`,
+      margin: `0`
     }
+
+    const iframeStyle = {
+      border: `10px solid black`
+    }
+
     return (
+      
       <div style={containerStyle}>
+        <p style={smallHeadingStyle}>It's {today}</p>
         <h1 style = {headingStyle}>Hello, Faye</h1>
-    <h2 style={smallHeadingStyle}>It's {today}</h2>
-    <p>{this.state.explanation}</p>
-    <Iframe url="https://calendar.google.com/calendar/embed?src=fayeelizabethburke%40gmail.com&ctz=Australia%2FBrisbane"
-        width="100%"
-        height="500px"
+
+
+
+    <Iframe style= {iframeStyle} url="https://calendar.google.com/calendar/embed?src=fayeelizabethburke%40gmail.com&ctz=Australia%2FBrisbane"
+        width="1200px"
+        height="700px"
         id="myId"
         className="myClassname"
         display="initial"
-        position="relative"/>
+        position="center"/>
       </div>
 
-
     )
+    
   }
 }
